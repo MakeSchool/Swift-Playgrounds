2ba66e6c8f461306e1c3b00b86c1ba171b731f0b
Abdulrahman AlZanki <aj@aalzanki.com>
2015-06-16 10:39:19 -0700

    Initial Var and Let tutorial

---
+++ VarAndLet.playground/Contents.swift
@@ -0,0 +1,42 @@
+/*:
+# Variables
+Variables associate a name to a value. Here's how you would declare a variable in Swift:
+*/
+
+var height = 6
+
+/*:
+We just declared a variable called `height` and set its initial value to `6`. Let's try changing that value.
+*/
+
+height = 10
+
+height
+
+/*:
+If you look at the right side of your screen, you will see the value of `height` has changed to 10.
+*/
+
+/*:
+# Constants
+Unlike variables, constants can not be changed. You can declare a constant using the word `let`:
+*/
+
+let pi = 3.14
+
+/*:
+Try changing `pi` by uncommenting the following line. You can uncomment it by deleting the `//`
+*/
+
+// pi = 2
+
+/*:
+Notice that you get an error on that line. Clicking on the error sign would show you the following: 
+Cannot assign to 'let' value 'pi'
+
+To get rid of the error, comment it again by adding `//` to the beginning of that line.
+*/
+
+/*:
+For more details on what we covered in this tutorial, please visit [Apple's Variables and Constants guide.](https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID310)
+*/
