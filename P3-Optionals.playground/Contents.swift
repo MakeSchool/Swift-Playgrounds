9018a2cea01aa6a448c93519f191829e4e9e77c6
Samara Trilling <samara.trilling@gmail.com>
2015-06-17 12:22:37 -0700

    P3 Add parallels to other languages and add some examples

--- P3-Optionals.playground/Contents.swift
+++ P3-Optionals.playground/Contents.swift
@@ -4,11 +4,11 @@ import UIKit
 //:
 //: ### Introduction to Optionals
 //:
-//: Sometimes, it's useful to express the fact that a variable may contain a value or no value at all. Swift allows us to do this with **optionals**. Optionals contain **either** a value **or** `nil` (meaning "no value").
+//: Sometimes, it's useful to express the fact that a variable may contain a value or no value at all. Swift allows us to do this with **optionals**. Optionals contain **either** a value **or** `nil` (meaning "no value"). Optionals are like boxes that may have something inside, or they may have nothing inside.
 //:
-//: Some languages, such as Objective-C, allow a variable to contain `nil` without its type expressly indicating that a value may be absent. Swift is much more strict about when variables can lack a value, and this is very helpful in avoiding bugs where we think we have a value when in fact we don't. Functions can return an optional to indicate when the operation has failed or they have no value to give back to you.
+//: Some languages, like Objective-C, Python and Java, allow a variable to be set to `nil` or `null` without its type expressly indicating that a value may be absent. Swift is much more strict about when variables can lack a value, and this is very helpful in avoiding bugs where we think we have a value when in fact we don't. Functions can return an optional to indicate when the operation has failed or they have no value to give back to you.
 //:
-//: Syntactically, we express the fact that a value is of optional type by appending a question mark (?) to the type name. For example, here is how we declare a variable of type `Int?` (read as "optional `Int`"):
+//: We express the fact that a value is of optional type by putting a question mark (?) at the end of the type name. For example, here is how we declare a variable of type `Int?` (read as "optional `Int`"):
 
 var maybeAnInt: Int? = 15
 
@@ -16,11 +16,12 @@ var maybeAnInt: Int? = 15
 
 maybeAnInt = nil
 
-//: Now `maybeAnInt` is `nil`, which is to say it has _no value_.
+//: Now `maybeAnInt` is `nil`, which is to say it has _no value_. If `maybeAnInt` had just been of type Int, we could not have set it to `nil`.
 
 //: ### Unwrapping an Optional
 //:
-//: When we get the value from an optional we say we "unwrap" it. We can test whether an optional has a value by comparing it to `nil`. If it is not equal to `nil`, it contains a value. We can then safely use **force unwrapping** to get its value. Force unwrapping is done by putting an exclamation point (!) after the name of the variable we want to unwrap. This assures Swift that the optional contains a value and that it is therefore safe to read. Here, we test whether `maybeAnInt` has a value, and conditionally print its value after force-unwrapping it.
+//: When we get the value from an optional, we say we "unwrap" it. This is like opening the box and seeing whether there's anything inside. We can test whether an optional has a value by comparing the whole thing to `nil`. If it is not equal to `nil`, it contains a value. Woo hoo!
+//: We can then safely use **force unwrapping** to get its value. Force unwrapping is done by putting an exclamation point (!) after the name of the variable we want to unwrap. This assures Swift that the optional contains a value and that it is therefore safe to read. Here, we test whether `maybeAnInt` has a value and if it does, we rip open the box (force unwrap) and print its value.
 
 if maybeAnInt != nil {
     println("maybeAnInt contains a value, and it is \(maybeAnInt!)")
@@ -28,11 +29,11 @@ if maybeAnInt != nil {
     println("maybeAnInt does not contain a value")
 }
 
-//: Try changing the value of `maybeAnInt` above. Notice that the message that gets printed depends on whether or not it contains a value.
+//: Try changing the value of `maybeAnInt` from `nil` to something else above. Notice that the message only gets printed if it contains a value.
 
 //: ### Optional Binding
 //:
-//: A more compact way of testing and acting on an optional value is **optional binding**, where we test for the presence of an object and give its value a new name in a narrower scope. Here, we "bind" the value of `maybeAnInt` (if present) to a new constant named `definitelyAnInt` and print it:
+//: A more compact way of testing and acting on an optional value is **optional binding**, where we test for the presence of an object and, if it exists, we create a new variable for this object in a narrower scope. Here, we "bind" the value of `maybeAnInt` (if present) to a new constant named `definitelyAnInt`, which only exists inside the `if/else` block, and print it:
 
 if let definitelyAnInt = maybeAnInt {
     println("maybeAnInt contains a value, and it is \(definitelyAnInt)")
@@ -40,11 +41,11 @@ if let definitelyAnInt = maybeAnInt {
     println("maybeAnInt does not contain a value")
 }
 
-//: Try changing the value of `maybeAnInt` again, and again take note that if it contains a value, the message indicates this and the local variable `definitelyAnInt` has the same value.
+//: Try changing the value of `maybeAnInt` again, and again take note that if it contains a value, the message indicates this and the local variable `definitelyAnInt` has the same value. If it doesn't contain a value, then `definitelyAnInt` won't be created.
 
 //: ### Implicitly Unwrapped Optionals
 //:
-//: Sometimes we want to indicate that an optional will always have a value when we need to read it. For example, this happens when we separate the declaration of an optional from the first time we set its value. To support this scenario, we can declare a variable as an **implicitly unwrapped optional**. Implicitly unwrapped optionals are declared by placing an exclamation point (!) after the type to indicate that they **can** contain `nil`, but must always have a value when they are read:
+//: Sometimes we want to indicate that an optional will always have a value when we need to read it. For example, this happens when we separate the declaration of an optional from the first time we set its value. The first time we declare the optional, it won't have a value, but we'll set it before we use it. To support this scenario, we can declare a variable as an **implicitly unwrapped optional**. Implicitly unwrapped optionals are declared by placing an exclamation point (!) after the type to indicate that they **can** contain `nil`, but must always have a value when they are read:
 
 var alwaysAString: String! = nil
 
@@ -70,22 +71,27 @@ println(alwaysAString)
 
 //let intDescription = maybeAnInt.description // Value of optional type 'Int?' not unwrapped!
 
-//: This restriction is lifted if you use a technique called **optional chaining**. Chaining allows you to try to call a method on an optional, which calls the method if the optional has a value, and returns `nil` if it does not. Chaining is performed by placing a question mark between the variable name and the dot, parenthesis, or bracket that follows it:
+//: But we're programmers and we like getting around the rules. You don't have to give Swift a non-optional if you use a technique called **optional chaining**. Chaining allows you to try to call a method on an optional, which calls the method if the optional has a value, and returns `nil` if it does not. Chaining is performed by placing a question mark between the variable name and the dot, parenthesis, or bracket that follows it:
 
 let optionalArray: [Int]? = [ 1, 2, 3, 4 ]
 let arrayLength = optionalArray?.count
 let firstElement = optionalArray?[0]
 
-//: Placing a `?` after the name `optionalArray` will cause Swift to check whether the variable is `nil` before attempting to call `count` or subscript into the array. The types of these expressions are optionals of the same type as the return type of method (so the call to `count`, which normally produces an `Int`, produces an `Int?` in this case).
+//: Placing a `?` after the name `optionalArray` will cause Swift to check whether the variable is `nil` before attempting to call `count` or access the array. The types of these expressions are optionals of the same type as the return type of method (so the call to `count`, which normally produces an `Int`, produces an `Int?` in this case). Phew! Still with us?
 //: 
 //: Set `optionalArray` to `nil` and observe how the output values change.
 
 //: ### The Nil Coalescing Operator
 //:
-//: Sometimes we want to use a default value in the place of an optional when it turns out to be `nil`. For example, we might want to provide a placeholder name for an object when its own `name` property is `nil`. Swift provides a way to do this very compactly: the nil-coalescing operator (`??`). When the optional to the left of the operator has a value, that value becomes the value of the expression. When the optional is `nil`, the value of the expression is the value on the right of the operator. Let's look at an example:
+//: Sometimes we want to use a default value in the place of an optional when it turns out to be `nil`. For example, we might want to provide a placeholder name for an object when its own `name` property is `nil`, or use 0 if an integer is `nil`.
+//: (If you know Java, you may recognize this as similar to the "conditional", or "ternary" operator.)
+
+//: Swift provides a way to do this very compactly: the nil-coalescing operator (`??`). When the optional to the left of the operator has a value, that value becomes the value of the expression. When the optional is `nil`, the value of the expression is the value on the right of the operator. Let's look at an example:
 
 let optionalString: String? = nil
 let petName = optionalString ?? "Fido"
+
+//: So if `optionalString` is not nil, we'll set `petName` to the value of `optionalString`. If it is nil, we'll set `petName` to "Fido".
 
 //: ### Recap
 //:
