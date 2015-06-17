5e3b19d666222ba6c68c5c98a0d03ba72e08203e
Samara Trilling <samara.trilling@gmail.com>
2015-06-17 11:08:29 -0700

    P0 Make text more conversational and add welcome

--- P0-Var-And-Let.playground/Contents.swift
+++ P0-Var-And-Let.playground/Contents.swift
@@ -1,4 +1,9 @@
 /*:
+# Welcome to Swift!
+This is the first in a series of short tutorials designed to get you up to speed with Swift-specific topics. You all have seen at least one programming language before, so these tutorials are going to focus on things that are new or special to Swift. We love Swift and hope you will too!
+*/
+
+/*:
 # Variables
 Variables associate a name to a value. Here's how you would declare a variable in Swift:
 */
@@ -14,7 +19,7 @@ height = 10
 height
 
 /*:
-If you look at the right side of your screen, you will see the value of `height` has changed to 10.
+If you look at the right side of your screen, you will see the value of `height` has changed to 10. As long as you define a variable with the keyword `var`, you can change its value later on.
 */
 
 /*:
@@ -22,17 +27,19 @@ If you look at the right side of your screen, you will see the value of `height`
 Unlike variables, constants can not be changed. You can declare a constant using the word `let`:
 */
 
-let pi = 3.14
+let pi = 3.14 // Mmm, pi.
 
 /*:
-Try changing `pi` by uncommenting the following line. You can uncomment it by deleting the `//`
+Suppose we want to try to bend the rules of mathematics and change the value of pi. Try changing `pi` by uncommenting the following line. You can uncomment it by deleting the `//`
 */
 
 // pi = 2
 
 /*:
-Notice that you get an error on that line. Clicking on the error sign would show you the following: 
-Cannot assign to 'let' value 'pi'
+Notice that you get an error on that line. This is because you cannot change the value of a constant. Clicking on the error sign would show you the following:
+`Cannot assign to 'let' value 'pi'`
+
+If you've declared a variable using `let`, you will not be able to change its value later on.
 
 To get rid of the error, comment it again by adding `//` to the beginning of that line.
 */
@@ -40,7 +47,7 @@ To get rid of the error, comment it again by adding `//` to the beginning of tha
 
 /*:
 ## Naming Variables and Constants
-When naming variables and constants, most programmers use a convention called Camel Case. Here's how you can follow it:
+When naming variables and constants, most Swift programmers use a convention called CamelCase. Here's how you can follow it:
 */
 
 // Correct
@@ -54,7 +61,7 @@ let Upcomingappversion = 1.1
 
 /*:
 The rules are as follows:
-* Start the first word with a lowercase letter.
+* Start the first word in the variable name with a lowercase letter.
 * Start any subsequent word with an uppercase letter.
 */
 
